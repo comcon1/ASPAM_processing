34bbddcaae442262fd1002f74a7321213e178341
comcon1 <comcon1@protonmail.com>
2021-04-06 23:51:09 +0300

    fix extract-rat for python3

--- src/extract-rat.py
+++ src/extract-rat.py
@@ -1,15 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+
 
 import sys, getopt, time, os
 import numpy as np
-from ratlib import *
+import pandas as pd
 
 helpmsg = """
 extractrat -i 15.12.2015 -t 9:00 -n 30 -c 1
 
  -i Start date in dd.mm.YYYY format
- -o Output filename
  -t Start time in HH:MM format
+ -f Input filename
+ -o Output filename
  -n Number of days to extract
  -c Channel to extract
 
@@ -23,9 +25,9 @@ def main():
 
     try:
         opts, args = getopt.getopt(sys.argv[1:], 'hf:i:t:n:c:o:')
-    except getopt.error, msg:
-        print msg
-        print 'for help use -h'
+    except getopt.error as msg:
+        print(msg)
+        print('for help use -h')
         sys.exit(2)
 
     stdate = sttime = fname = outf =''
@@ -34,7 +36,7 @@ def main():
 
     for o, a in opts:
         if o == '-h':
-          print helpmsg
+          print(helpmsg)
           sys.exit(0)
         elif o == '-i':
             stdate = a
@@ -51,46 +53,46 @@ def main():
 
     if stdate == '' or sttime == '' or ndays == 0 or nchan == 0 or \
             fname == '' or outf == '':
-        print 'for help use -h'
+        print('for help use -h')
         sys.exit(2)
-    
+
     try:
         timestart = time.strptime(stdate + ' ' + sttime, '%d.%m.%Y %H:%M')
         tstampstart = time.mktime(timestart)
     except:
-        print 'Bad date or time format.'
+        print('Bad date or time format.')
         sys.exit(3)
 
     if not os.path.isfile(fname):
-        print 'Wrong file name.'
+        print('Wrong file name.')
         sys.exit(41)
 
     try:
-        print 'Reading data from ', fname
-        a = np.loadtxt(fname)
+        print('Reading data from ', fname)
+        a = pd.read_csv(fname, header=None, sep='\s+', comment='#').values
     except:
-        print 'Wrong file format. Check the last line.'
+        print('Wrong file format. Check the last line.')
         sys.exit(42)
 
     if a[:,0].min() > tstampstart:
         TS = time.strftime('%d.%m.%Y %H:%M', time.localtime(a[:,0].min()) )
-        print 'File starts from %s' % (TS)
+        print('File starts from %s' % (TS) )
         TS = time.strftime('%d.%m.%Y %H:%M', time.localtime(tstampstart) )
-        print 'And you request data from %s' % (TS)
+        print('And you request data from %s' % (TS) )
         sys.exit(51)
 
     tstampstop = min( tstampstart + 24*3600*ndays, a[:,0].max() )
-    print 'Requested interval from %s to %s for rat No. %d.' % \
-        ( time2str(tstampstart), time2str(tstampstop), nchan )
+    print( 'Requested interval from %s to %s for rat No. %d.' %
+        ( time2str(tstampstart), time2str(tstampstop), nchan ) )
 
     i = a[:,0].searchsorted(tstampstart)
     j = a[:,0].searchsorted(tstampstop)
 
     np.savetxt(outf, a[i:j,[0,nchan]], fmt='%d')
 
-    print 'Separate rat data saved..'
+    print('Separate rat data saved..')
 
 if __name__ == '__main__':
     main()
 
-time = sys.argv[0] 
+time = sys.argv[0]
